d739a443bc7271243b1dab4d82a2bd1c1e89b779
brock patterson <brockpatterson@gmail.com>
2017-04-11 09:50:19 -0700

    adding 9_DOM Scripting Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,31 @@
+    var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
-        //getelementsbyclassname is a selector that
-        //returns list of element nodes which use index notation
-    var revealPoint = function(index) {
+    var animatePoints = function(points) {
+     var revealPoint = function(index) {
       points[index].style.opacity = 1;
       points[index].style.transform = "scaleX(1) translateY(0)";
       points[index].style.msTransform = "scaleX(1) translateY(0)";
       points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
+    
     for(var i =0; i<points.length; i++){
         revealPoint(i);
     }
+    
+    };
+    
+    window.onload = function() {
+      
+      if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+      var sellingPoints = document.getElementsByClassName('selling-points')[0];
+      var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      
+      window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+    };
