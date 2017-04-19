9b0a81f967111c674869fb961ac6dc248960c7d7
brock patterson <brockpatterson@gmail.com>
2017-04-19 14:11:12 -0700

    adding 12_DOM_Scripting

--- scripts/album.js
+++ scripts/album.js
@@ -43,16 +43,14 @@ var albumRadiohead = {
    ]
 };
 
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
      ;
-
     return template;
 };
 
@@ -63,12 +61,10 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
-
     albumSongList.innerHTML = '';
 
     for (var i = 0; i < album.songs.length; i++) {
@@ -76,8 +72,26 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumRadiohead);
+
+songListContainer.addEventListener('mouseover',function(event){
+  if(event.target.parentElement.className === 'album-view-song-item'){
+    event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+  }
+});
+
+for (var i = 0; i < songRows.length; i++) {
+     songRows[i].addEventListener('mouseleave', function(event) {
+       this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+ 
+     });
+ }
 
     var albums = [albumPicasso, albumMarconi, albumRadiohead];
     var index = 1;
