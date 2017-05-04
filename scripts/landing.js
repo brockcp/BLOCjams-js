365adb6af0a19b5ea8c06a04724757d0e782a0ca
brock patterson <brockpatterson@gmail.com>
2017-05-03 19:41:00 -0700

    adding 16_jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,27 @@
+var animatePoints = function() {
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+};
+  $.each($('.point'), revealPoint);
+};
 
-    var points = document.getElementsByClassName('point');
-        //getelementsbyclassname is a selector that
-        //returns list of element nodes which use index notation
-    var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    for(var i =0; i<points.length; i++){
-        revealPoint(i);
+
+
+$(window).load(function() {
+
+  if ($(window).height() > 950) {
+     animatePoints();
+ }
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event) {
+
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
+ });
+});
