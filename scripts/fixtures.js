7daf11538a5365160d6f67ac775564dce9bf0dc4
brock patterson <brockpatterson@gmail.com>
2017-05-09 10:01:22 -0700

    adding 19_jQuery

---
+++ scripts/fixtures.js
@@ -0,0 +1,42 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+var albumRadiohead = {
+   title: 'OK Computer',
+   artist: 'Radiohead',
+   label: 'Indy',
+   year: '1997',
+   albumArtUrl: 'assets/images/album_covers/20.png',
+   songs: [
+       {title: 'The Tourist', duration:'3:43'},
+       {title: 'Let Down', duration:'4;44'},
+       {title: 'Karma Police', duration:'6:23'},
+       {title: 'Fitter Happier', duration:'5:32'},
+       {title: 'Electioneering', duration:'5:65'},
+   ]
+};
