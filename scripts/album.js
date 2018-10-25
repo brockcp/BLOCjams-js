b60eae982a46f877a5ac0bebd5244a9646651e5c
brockcp <brockpatterson@gmail.com>
2018-10-24 20:03:32 -0700

    added songlist-not functional

--- scripts/album.js
+++ scripts/album.js
@@ -1,20 +1,40 @@
 window.onload = function(){ //necessary if no local albums
 
-var urlPassedAlbum = window.location.hash.substr(1);
+  var urlPassedAlbum = window.location.hash.substr(1);
 
-for(var i=0, len=albums.length; i<len; i++){
-  if(albums[i].id == urlPassedAlbum){
-    var chosenAlbum = albums[i];
+  for(var i=0, len=albums.length; i<len; i++){
+    if(albums[i].title == urlPassedAlbum){
+      var chosenAlbum = albums[i];
+    }
   }
-}
 
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+  var albumTitle = document.getElementsByClassName('album-view-title')[0];
+  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-albumTitle.innerHTML = chosenAlbum.title;
-albumArtist.innerHTML = chosenAlbum.artist;
-albumReleaseInfo.innerHTML = chosenAlbum.year + ' ' + chosenAlbum.label;
-document.getElementById("fff").src = chosenAlbum.albumArtUrl;
+  albumTitle.innerHTML = chosenAlbum.title;
+  albumArtist.innerHTML = chosenAlbum.artist;
+  albumReleaseInfo.innerHTML = chosenAlbum.year + ' ' + chosenAlbum.label;
+  document.getElementById("fff").src = chosenAlbum.albumArtUrl;
+
+
+  var createSongRow = function(songNumber, songName, songLength) {
+      //HTML TARGET IS <table class="album-view-song-list">
+      var template =
+         '<tr class="album-view-song-item">'
+       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+       + '  <td class="song-item-title">' + songName + '</td>'
+       + '  <td class="song-item-duration">' + songLength + '</td>'
+       + '</tr>'
+       ;
+      return template;
+  };
+
+  albumSongList.innerHTML = '';
+  for(var j = 0; j<chosenAlbum.songs.length; j++){
+    albumSongList.innerHTML += createSongRow(j+1, chosenAlbum.songs[j].title, chosenAlbum.songs[j].duration);
+  }
+
 
 }
--- scripts/collection.js
+++ scripts/collection.js
@@ -1,13 +1,13 @@
-albumItemTemplate = '';
 
+albumItemTemplate = '';
 for(var i=0, len=albums.length; i<len; i++){
   var albumBuild =
      '<div class="collection-album-container column fourth">'
-   + '  <a href="album.html#' + albums[i].id +'">'
+   + '  <a href="album.html#' + albums[i].title +'">'
    + '    <img src="' + albums[i].albumArtUrl + '"/>'
    + '    <div class="collection-album-info caption">'
    + '      <p>'
-   + '        <a class="album-name" href="album.html#' + albums[i].id +'">' + albums[i].title + '</a>'
+   + '        <a class="album-name" href="album.html#' + albums[i].title +'">' + albums[i].title + '</a>'
    + '        <br/>'
    + '        <a class="">' + albums[i].artist + '</a>'
    + '        <br/>'
@@ -20,7 +20,5 @@ for(var i=0, len=albums.length; i<len; i++){
    ;
    albumItemTemplate = albumItemTemplate + albumBuild;
  }
-
 var collectionContainer = document.getElementsByClassName("album-covers")[0];
-
 collectionContainer.innerHTML += albumItemTemplate;
